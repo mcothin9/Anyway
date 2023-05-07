2caa7c0c9dbb758edd5286dfecde7fd416244c9e
MJ-rocket <127004932+MJ-rocket@users.noreply.github.com>
2023-05-07 16:01:36 +1000

    Connect UserLoginUI from view to view controller

--- Anyway/Anyway/Controller/UserLoginViewController.swift
+++ Anyway/Anyway/Controller/UserLoginViewController.swift
@@ -9,9 +9,22 @@ import UIKit
 
 class UserLoginViewController: UIViewController {
 
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var ageTextField: UITextField!
+    @IBOutlet weak var genderTextField: UITextField!
+    @IBOutlet weak var mobileTextField: UITextField!
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        
+        
+        
     }
 
 
